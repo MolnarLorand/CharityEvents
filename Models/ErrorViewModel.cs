293a55f7443b47318dda4a3a9d494579b5369e8f
MolnarLorand <andrevzo84@gmail.com>
2022-04-14 11:45:54 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CharityEvents.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
