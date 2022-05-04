1032aedf116fb2b4dfaf52c6e7bf4fa68b95fc46
MolnarLorand <andrevzo84@gmail.com>
2022-05-04 15:18:26 +0300

    Identity

--- Migrations/20220504121349_Identity.Designer.cs
+++ Migrations/20220504121349_Identity.Designer.cs
@@ -4,14 +4,16 @@ using CharityEvents.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CharityEvents.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220504121349_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharityEvents.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        //additional prop to identityuser-> add col in db
+        [Display(Name = "Nume Complet")]
+        public string FullName { get; set; }
+    }
+}
